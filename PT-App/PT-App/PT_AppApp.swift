1112b20a25574ca72507fe414429ca344e802f25
cole.b.david <cole.b.david@gmail.com>
2025-10-04 19:04:03 +0100

    Initial commit created project in Xcode

---
+++ PT-App/PT-App/PT_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PT_AppApp.swift
+//  PT-App
+//
+//  Created by David Cole on 04/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PT_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PT-App/PT-AppTests/PT_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PT_AppTests.swift
+//  PT-AppTests
+//
+//  Created by David Cole on 04/10/2025.
+//
+
+import Testing
+@testable import PT_App
+
+struct PT_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
